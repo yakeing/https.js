bc7b570c067f08e516f0fa525495dfc95a1d61d7
yakeing <6356091+yakeing@users.noreply.github.com>
2020-12-18 23:13:04 +0800

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,20 +1,10 @@
-const core = require('@actions/core');
 const https = require('./src/https.js');
-async function run() {
-  try {
-      var url = "https://example.com";
-      var res_function = function(res, status) {
-        if( status != 200){ //ok
-          // responseText
-          return 0;
-        }
-      }
-
-      var ARGUMENTS = {URL:url, RES_FUNCTION:res_function, HEADER:{'Content-type':'application/json;charset=UTF-8'}};
-      https(ARGUMENTS);
-  }
-  catch (error) {
-    core.setFailed(error.message);
+var url = "https://example.com";
+var res_function = function(res, status) {
+  if( status != 200){ //ok
+    // responseText
   }
 }
-run()
+
+var ARGUMENTS = {URL:url, RES_FUNCTION:res_function, HEADER:{'Content-type':'application/json;charset=UTF-8'}};
+https(ARGUMENTS);
