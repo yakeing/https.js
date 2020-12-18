5c202aee9f7568009b3dfb2b396fd251aa7579de
yakeing <6356091+yakeing@users.noreply.github.com>
2020-12-18 17:48:16 +0800

    Create index.test.js

---
+++ index.test.js
@@ -0,0 +1,18 @@
+import { https } from 'src/https';
+async function run() {
+  try {
+      var url = "https://example.com";
+      var res_function = function(res, status) {
+        if( status != 200){ //ok
+          // responseText
+        }
+      }
+
+      var ARGUMENTS = {URL:url, RES_FUNCTION:res_function, HEADER:{'Content-type':'application/json;charset=UTF-8'}};
+      https(ARGUMENTS);
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run()
