8c2ad83ab67b87febbe9989f8e780570b41fca86
yakeing <6356091+yakeing@users.noreply.github.com>
2021-03-30 17:45:59 +0800

    Update run-test.js

--- test/run-test.js
+++ test/run-test.js
@@ -3,7 +3,7 @@ const https = require('../src/https.js');
 var url = 'https://example.com';
 var res_function = function(res, status) {
   if( status == 200){ //ok
-  	// responseText
+  	//responseText
   }
 }
 var ARGUMENTS = {URL:url, RES_FUNCTION:res_function, HEADER:{'Content-type':'text/html; charset=UTF-8'}};
